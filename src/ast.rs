f5399da2d0de0c0393aa4e93642658dabd648c1f
nassorc <macrossan17@gmail.com>
2024-02-06 00:56:33 -0700

    Update ast.rs

--- src/ast.rs
+++ src/ast.rs
@@ -1,31 +1,52 @@
 #[derive(Debug, Clone)]
-pub enum NodeType {
+pub enum Statement {
     LetStatement(LetStatement),
-    ExpressionStatement(ExpressionType),
+    ReturnStatement(ReturnStatement),
+    ExpressionStatement(Expression),
     Nil
 }
 
-#[derive(Debug, Clone)]
-pub enum ExpressionType {
+#[derive(Debug, Clone, PartialEq)]
+pub enum Expression {
     Ident(Identifier),
     Int(Integer),
+    Prefix(PrefixExpression),
+    Infix(InfixExpression),
     Nil
 }
 
 #[derive(Clone)]
 pub struct Program {
-    pub statements: Vec<NodeType>
+    pub statements: Vec<Statement>
 }
 
 #[derive(Debug, Clone)]
 pub struct LetStatement {
     pub name: Identifier,
-    pub value: ExpressionType
+    pub value: Expression
 }
 
 #[derive(Debug, Clone)]
+pub struct ReturnStatement {
+    pub value: Expression
+}
+
+#[derive(Debug, Clone, PartialEq)]
 pub struct Identifier(pub String);
 
-
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Integer(pub i32);
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct PrefixExpression {
+    pub op: String,
+    pub right: Box<Expression>
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct InfixExpression {
+    pub op: String,
+    pub right: Box<Expression>,
+    pub left: Box<Expression
+    >
+}
