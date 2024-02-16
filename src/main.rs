045b9a3787571e39c8d9bdf2fac0ce14f5e077a1
nassorc <macrossan17@gmail.com>
2024-02-16 03:36:25 -0700

    Include module in main

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,77 @@
 #![allow(unused)]
 
+mod app;
+mod evaluator;
 mod lexer;
 mod parser;
-mod evaluator;
-mod app;
+mod utils;
 
+use std::cell::RefCell;
 use std::ops::Deref;
 use std::rc::Rc;
-use std::cell::RefCell;
 
 use app::App;
-use lexer::Lexer;
-use parser::Parser;
 use evaluator::{environment::Environment, object::Object, *};
-
+use lexer::Lexer;
+use parser::{
+    ast::{Identifier, Integer, LetStatement, Node},
+    Parser,
+};
 
 fn main() {
-    
-    dbg!(&App::new("
-        let global = 100;
-        let a = fn(x) { 
-            if (false) {
-                return 0;
-            } else {
-                let a = 10; 
-                return a + x + global; 
-            }
-        };
-        a(1) + 4
-    ").execute());
+    let input = "let a = if (true) { 10 + 2; 2; } else { 5 }";
+    let input = "let a = if (false) { 10 + 2; return 99; } else { return 55; }; a;";
+    let input = "
+    let global = 100;
+    let a = fn(x) { 
+        if (false) {
+            return 0;
+        } else {
+            let a = 10; 
+            return a + x + global; 
+        }
+    };
+    a(1) + 4
+    ";
 
+    // dbg!(&App::new(
+    //     "
+    //     let global = 100;
+    //     let a = fn(x) {
+    //         if (false) {
+    //             return 0;
+    //         } else {
+    //             let a = 10;
+    //             return a + x + global;
+    //         }
+    //     };
+    //     a(1) + 4
+    // "
+    // )
+    // .execute());
 
-    // let input = "let a = if (true) { 10 + 2; 2; } else { 5 }";
-    // let input = "let a = if (false) { 10 + 2; return 99; } else { return 55; }; a;";
-    // let input = "
-    // let global = 100;
-    // let a = fn(x) { 
-    //     if (false) {
-    //         return 0;
-    //     } else {
-    //         let a = 10; 
-    //         return a + x + global; 
-    //     }
-    // };
-    // a(1) + 4
-    // ";
-    // let lexer = Lexer::new(input);
-    // let mut parser = Parser::new(lexer);
+    let input = "
+    x + y + myVar;
+    10
+    let a = 111;
+    let b = 222;
+    100; 200;
+    return true;
+    if (10) { 10; 20; let a = 200; } else { 2; }
+    !false;
+    let myFunc = fn (a, b) { return -10 + 2; };
+    myFunc(-10, false);
+    a();
+    ";
+    let input = "if (false) { 5 }";
+    let lexer = Lexer::new(input);
+    let mut parser = Parser::new(lexer);
 
-    // let prog = parser.parse_program();
-    // let mut env = Environment::new();
+    let prog = parser.parse_program();
+    println!("{}", prog.to_string());
+    let mut env = Environment::new();
 
     // dbg!(&prog);
     // dbg!(&env);
-    // dbg!(eval(&prog, Rc::clone(&env)));
+    dbg!(eval(&prog, Rc::clone(&env)));
 }
-
